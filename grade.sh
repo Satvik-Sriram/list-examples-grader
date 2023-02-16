7ecfc22bb0e1b633aad44b798667d409fe15c4d3
SatvikS7 <79161050+SatvikS7@users.noreply.github.com>
2023-02-16 15:09:22 -0800

    added stuff

--- grade.sh
+++ grade.sh
@@ -1,5 +1,20 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+if [[ -f ListExamples.java ]]
+then 
+    echo 'File Found'
+else 
+    echo 'Need ListExamples.java'
+    exit 1
+fi 
+cp ../TestListExamples.java TestListExamples.java
+cp -r ../lib lib
+echo 'files copied'
+javac -cp $CPATH *.java
+echo 'file compilied'
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > result.txt
+echo 'test complete'
